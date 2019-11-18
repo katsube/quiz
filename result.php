e05317dfc9310046b8eb4e648ab98bf76884a111
Makito Katsube <katsubemakito@gmail.com>
2019-11-18 15:38:57 +0900

    add validation in result.php

--- result.php
+++ result.php
@@ -2,8 +2,15 @@
 require_once('common.php');
 
 // 引数（クエリー）を受け取る
-$qid    = $_GET['qid'];
+$qid    = isset($_GET['qid'])? $_GET['qid']:-1; 
 $answer = $_GET['answer'];
+
+// Validation
+if($qid == -1 || !is_numeric($qid) || !((0<=$qid) && ($qid<count($question))) ){
+	echo 'error: $qid invalid';
+	exit(1);
+}
+
 
 // 正解か判定
 if( $question[$qid][1] == $answer ){
