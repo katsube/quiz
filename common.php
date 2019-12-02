72c4029c913418ce51d8838705cc70faf8bd685a
Makito Katsube <katsubemakito@gmail.com>
2019-12-02 15:03:58 +0900

    modify reault.php, common.php

--- common.php
+++ common.php
@@ -1,9 +1,4 @@
 <?php
-$question = [
-	["富士山の標高は何メートル？", 3776],
-	["下は大火事、上は洪水、それなんだ？", '風呂'],
-	["パンはパンでも食べられないパンは？", 'フライパン']
-];
 
 function getDB1($sql, $param=[]){
 	$dsn  = 'mysql:dbname=quizdb;host=localhost';
