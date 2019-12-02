6fbc285da637e5dbb312c820ff10b895fc3407a6
Makito Katsube <katsubemakito@gmail.com>
2019-12-02 14:53:14 +0900

    read DB

--- common.php
+++ common.php
@@ -4,3 +4,16 @@ $question = [
 	["下は大火事、上は洪水、それなんだ？", '風呂'],
 	["パンはパンでも食べられないパンは？", 'フライパン']
 ];
+
+function getDB1($sql, $param=[]){
+	$dsn  = 'mysql:dbname=quizdb;host=localhost';
+	$user = 'senpai';
+	$pw   = 'indocurry';
+
+	$dbh = new PDO($dsn, $user, $pw);
+	$sth = $dbh->prepare($sql);
+	$sth->execute($param);
+
+	return( $sth->fetch(PDO::FETCH_ASSOC) );
+}
+
--- question.php
+++ question.php
@@ -1,7 +1,14 @@
 <?php
 require_once('common.php');
 
-$i = rand(0, count($question)-1);
+// idの最大値を取得（=問題数）
+$data = getDB1('select max(id) as maxid from Question');
+
+// 出題する問題を決定
+$i = rand(1, $data['maxid']);
+
+// 問題文を取得
+$data = getDB1('select question from Question where id=?', [$i]);
 ?>
 <!DOCTYPE html>
 <html>
@@ -11,7 +18,7 @@ $i = rand(0, count($question)-1);
 </head>
 <body>
     <h1>Question</h1>
-	<?= $question[$i][0] ?>
+	<?= $data['question'] ?>
 
 	<form action="result.php">
 		<input type="hidden" name="qid" value="<?= $i ?>">
